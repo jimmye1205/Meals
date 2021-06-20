9b87180a45a6976fecb47e54faa5208b5603129d
jimmye1205 <jimmye1205@gmail.com>
2021-06-20 02:18:46 -0700

    again

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is the new meals to go</Text>
+      <Text>This is the new meals to go again</Text>
       <StatusBar style="auto" />
     </View>
   );
